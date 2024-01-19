ef226fcee37f3f9301ac7dfa19ea319a33110369
nuria <nrioolm101@g.educaand.es>
2024-01-19 10:08:10 +0100

    Tarjetas

--- src/main/java/daw/BBDD.java
+++ src/main/java/daw/BBDD.java
@@ -4,6 +4,8 @@
  */
 package daw;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 
 /**
@@ -14,35 +16,47 @@ public class BBDD {
 
     public static ArrayList listaDeTPVs() {
         ArrayList<TPV> listaTPVs = new ArrayList<>();
-        
+
         //CREACIÓN DE LAS 4 TPV
-        
         listaTPVs.add(new TPV("Estepona Avd.JuanCarlos"));
-        
+
         listaTPVs.add(new TPV("Manilva Calle Mar"));
-        
+
         listaTPVs.add(new TPV("Estepona Avd.España"));
-        
+
         listaTPVs.add(new TPV("Cancelada Avd.Las Palmeras"));
-        
-        for(int i = 0;i<listaTPVs.size();i++){
-        System.out.println(listaTPVs.get(i).toString());
+
+        for (int i = 0; i < listaTPVs.size(); i++) {
+            System.out.println(listaTPVs.get(i).toString());
         }
-        
+
         return listaTPVs;
     }
-    
+
     public static ArrayList listaDeTarjetas() {
         ArrayList<Tarjeta> listaTarjetas = new ArrayList<>();
+
+        //TARJETAS
+        listaTarjetas.add(new Tarjeta(1123, 456, "Paquito Sánchez", 114.89, LocalDate.of(2060, 5, 13)));
+
+        listaTarjetas.add(new Tarjeta(8997, 113, "MariLoli García", 30.6, LocalDate.of(2025, 6, 1)));
+
+        listaTarjetas.add(new Tarjeta(5559, 496, "Jesús Hernández", 500_000_000, LocalDate.of(2040, 12, 8)));
+
+        listaTarjetas.add(new Tarjeta(6643, 224, "Rosario Del Castillo", 456, LocalDate.of(2036, 2, 3)));
+
+        listaTarjetas.add(new Tarjeta(9213, 590, "Fernando Fernández", 749, LocalDate.of(3333, 3, 4)));
+            
+        listaTarjetas.add(new Tarjeta(6656, 777, "Inés González", 121212, LocalDate.of(2040, 11, 11)));
         
+        listaTarjetas.add(new Tarjeta(4398, 130, "Sergio Pérez", 700, LocalDate.of(2029, 9, 15)));
         
+        listaTarjetas.add(new Tarjeta(8966, 754, "Rafael Puentes", 2212, LocalDate.of(2045, 7, 23)));
         
-        
-        
-        
+        listaTarjetas.add(new Tarjeta(0773, 263, "Clara Palas", 3344, LocalDate.of(2026, 10, 31)));
         return listaTarjetas;
     }
-    
+
     public static ArrayList listaDeProductos() {
         ArrayList<Producto> listaProductos = new ArrayList<>();
 
@@ -127,7 +141,7 @@ public class BBDD {
         listaProductos.add(new Producto(129, "Miel y mostaza",
                 "Deliciosa salsa con un toque dulce", 0.5,
                 CATEGORIA.otros, SUBCATEGORIA.otros, 100, IVA.ivaOtros));
-        
+
         return listaProductos;
     }
 }
